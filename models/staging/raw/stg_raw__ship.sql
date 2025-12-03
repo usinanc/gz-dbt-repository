8b850ff6949eeecd05ceb74d1f210fdb793d90da
umutsinancaglar1048@gmail.com <umutsinancaglar1048@gmail.com>
2025-12-03 16:06:22 +0000

    ship düzelttim

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,22 +1,14 @@
-with 
-
-source as (
-
-    select * from {{ source('raw', 'ship') }}
-
-),
-
+with
+	source as (
+		select * from {{ source('raw', 'ship') }}
+	),
 renamed as (
-
-    select
-        orders_id,
-        shipping_fee,
-        shipping_fee_1,
-        logcost,
-        ship_cost
-
-    from source
-
+	select
+		orders_id,
+		shipping_fee,
+		logcost AS log_cost,
+		CAST(ship_cost AS INT64) AS ship_cost
+	from source
 )
 
 select * from renamed
